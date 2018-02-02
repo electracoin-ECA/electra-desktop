cf998761886059300eafaa7bf98613c6df70eede
hloudin <hyjaz.loudin@gmail.com>
2018-02-02 16:10:28 -0500

    make call to rpc getstakinginfo instead of getmininginfo and show the output on overview view

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -26,7 +26,7 @@
           STAKING
         </div>
         <div class="status-item__icon">
-          <icon v-if="stakingStatus.netstakeweight > 0" icon="circle" width="11" height="11" class="icon active"></icon>
+          <icon v-if="stakingStatus.enabled > 0" icon="circle" width="11" height="11" class="icon active"></icon>
           <icon v-else icon="circle-o" width="11" height="11" class="icon"></icon>
         </div>
       </div>
@@ -57,51 +57,51 @@
   electra-logo_padding = 0 15px
 
   status-bar_padding = 0 15px
-  
+
   status-item_padding = 0 7px
   status-item__label_color = rgba(255,255,255,0.8)
   status-item__icon_color = #fff
   status-item__icon_opacity = 0.5
   status-item__icon--active_opacity = 1
-  
+
   .app-header
     display flex
     width 100%
     height 100%
-    
-    .electra-logo 
+
+    .electra-logo
       display flex
       width auto
       height 100%
       padding electra-logo_padding
-      
-      img 
+
+      img
         width auto
         height 55%
         margin auto 0
-        
-    .status-bar 
+
+    .status-bar
       display flex
       height 100%
       padding status-bar_padding
-      
+
   .status-item
     position relative
     top -2px
     margin auto 0
     padding status-item_padding
-    
+
     &__label
       display inline-block
       color status-item__label_color
-      
+
     &__icon
       display inline-block
       margin-left 5px
       color status-item__icon_color
-      
+
       .icon
-        opacity status-item__icon_opacity       
+        opacity status-item__icon_opacity
         &.active
           opacity status-item__icon--active_opacity
 
