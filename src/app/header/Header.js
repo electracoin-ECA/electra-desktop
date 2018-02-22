de3758a6cc65c685cacfe8c84dbe1596ee0bee61
tunguyennnnn <conghoa522@gmail.com>
2018-02-21 21:29:26 -0500

    add wallet info

--- src/app/header/Header.js
+++ src/app/header/Header.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import WalletInfo from './components/WalletInfo'
 
 export default class Header extends React.Component {
   render () {
     return (
       <div class='header-container'>
         <div class='logo'>
+          <img src='https://cdn.discordapp.com/attachments/414963844587323392/416053920930988032/electra-logo.png'></img>
         </div>
         <div class='wallet-info-container'>
-          
+          <WalletInfo label={'Active connections'} info={'294'} />
+          <WalletInfo label={'Wallet is currently'} info={'Online'} />
+          <WalletInfo label={'Days until staking reward'} info={'13 days'} />
         </div>
       </div>
     )
