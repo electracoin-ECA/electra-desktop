141321db16e877df04cb586607521cb7f54fcb50
tunguyennnnn <conghoa522@gmail.com>
2018-02-25 02:02:01 -0500

    fixed header epic

--- src/app/header/epics.js
+++ src/app/header/epics.js
@@ -33,24 +33,4 @@ export function getStakingInfo (action$, store) {
         type: ActionNames.FAILED_TO_RETRIEVE_STAKING_INFO
       })
     })
-  // .map(() => {
-  //   console.log('reachhhhhh map')
-  //   store.getState().electraReducer.electraJs}) // get electraJs object from the store
-  // //.filter(electraJs => electraJs) // check if electraJs exists
-  // .map(electraJs => electraJs.wallet.getStakingInfo()) // get staking info
-  // .switchMap(stakingInfo => {
-  //   return stakingInfo
-  //   .then(data => ({
-  //       type: ActionNames.RECEIVED_STAKING_INFO,
-  //       payload: {
-  //         ...data
-  //       }
-  //     })
-  // )})
-  // .catch (err => {
-  //   console.log('react here')
-  //   return Observable.of({
-  //     type: ActionNames.FAILED_TO_RETRIEVE_STAKING_INFO
-  //   })
-  // })
 }
