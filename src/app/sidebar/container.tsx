1801d76abb4a98c974db391e2d6c93fb80d98f5c
hloudin <hyjaz.loudin@gmail.com>
2018-03-03 21:29:49 -0500

    add sidebar styling

--- src/app/sidebar/container.tsx
+++ src/app/sidebar/container.tsx
@@ -3,7 +3,11 @@ import * as React from 'react';
 export default class Sidebar extends React.Component<any, any> {
   render() {
     return (
-      <div>Side bar</div>
+      <div className="c-sidebar">
+        <div className="c-sidebar__nav">
+            ((NAV))
+        </div>
+      </div>
     )
   }
 };
