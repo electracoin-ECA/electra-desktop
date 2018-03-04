e170153cb8c2b228e0ea5de267105eb2c1ff9252
hloudin <hyjaz.loudin@gmail.com>
2018-03-04 10:25:27 -0500

    fix header, update toolkit, and update sidebar

--- src/app/sidebar/container.tsx
+++ src/app/sidebar/container.tsx
@@ -4,9 +4,17 @@ export default class Sidebar extends React.Component<any, any> {
   render() {
     return (
       <div className="c-sidebar">
-        <div className="c-sidebar__nav">
-            ((NAV))
-        </div>
+          <div className="c-sidebar__nav">
+              <nav className="c-nav">
+                  <div className="c-nav__title">Navigation</div>
+                  <ul className="c-nav__items">
+                      <li>Overview</li>
+                      <li>Payments</li>
+                      <li className="active">Transactions</li>
+                      <li>Address book</li>
+                  </ul>
+              </nav>
+          </div>
       </div>
     )
   }
