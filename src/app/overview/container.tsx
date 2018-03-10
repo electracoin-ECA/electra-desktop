fd95c7587d418ba3f8d86454d7cb4a824ec4b3b3
hloudin <hyjaz.loudin@gmail.com>
2018-03-10 17:07:24 -0500

    fix react typedefs

--- src/app/overview/container.tsx
+++ src/app/overview/container.tsx
@@ -2,8 +2,7 @@ import * as React from 'react'
 import { OverviewData } from './types'
 
 export default class Overview extends React.Component<OverviewData, any> {
-  // tslint:disable-next-line:typedef
-  render() {
+  public render(): any {
     return (
       <div>Overview</div>
     )
