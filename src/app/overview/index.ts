6c8f00fb60d95ff2910296d0edfd9bb0774d484e
Powerade <jmsolivdev@gmail.com>
2018-03-11 21:09:20 +0000

    add global balance to state

--- src/app/overview/index.ts
+++ src/app/overview/index.ts
@@ -1,3 +1,7 @@
+import * as OverviewNames from './action-names'
+import * as OverviewActions from './actions'
 import Overview from './container'
+import * as OverviewEpic from './epics'
+import overviewReducer from './reducer'
 
-export { Overview }
+export { Overview, OverviewNames, OverviewActions, overviewReducer, OverviewEpic }
