5789e34a9eef86c89d92b2b580f75edd48a7df64
Powerade <jmsolivdev@gmail.com>
2018-03-12 23:32:56 +0000

    add current prices to state

--- src/app/overview/actions.ts
+++ src/app/overview/actions.ts
@@ -1,8 +1,20 @@
 import * as ActionNames from './action-names'
-import { GetGlobalBalance } from './types'
+import { GetCurrentPriceBTC, GetCurrentPriceUSD, GetGlobalBalance  } from './types'
 
 export function getGlobalBalance(): GetGlobalBalance {
     return {
         type: ActionNames.GET_GLOBAL_BALANCE
+    }
+}
+
+export function getCurrentPriceInUSD(): GetCurrentPriceUSD {
+    return {
+        type: ActionNames.GET_CURRENT_PRICE_USD
+    }
+}
+
+export function getCurrentPriceInBTC(): GetCurrentPriceBTC {
+    return {
+        type: ActionNames.GET_CURRENT_PRICE_BTC
     }
 }
--- src/app/overview/container.tsx
+++ src/app/overview/container.tsx
@@ -4,17 +4,23 @@ import { DispatchProps } from './types'
 const { connect } = require('react-redux')
 import { bindActionCreators, Dispatch } from 'redux'
 
-import { getGlobalBalance } from './actions'
+import { getCurrentPriceInBTC, getCurrentPriceInUSD } from './actions'
 
 // tslint:disable-next-line:typedef
 const mapDispatchToProps = (dispatch: Dispatch<{}>): DispatchProps =>
   bindActionCreators({
-    getGlobalBalance
+    getCurrentPriceInBTC,
+    getCurrentPriceInUSD
 // tslint:disable-next-line:align
 }, dispatch)
 
 @connect(null, mapDispatchToProps)
-export default class Overview extends React.Component {
+export default class Overview extends React.Component<DispatchProps, any> {
+  componentDidMount(): void {
+    this.props.getCurrentPriceInUSD()
+    this.props.getCurrentPriceInBTC()
+  }
+
   public render(): any {
     return (
       <div>Overview</div>
