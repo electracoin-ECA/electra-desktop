e3f3ebdc18b90aa156b039a98667f6d439030083
Powerade <jmsolivdev@gmail.com>
2018-03-17 19:39:33 +0000

    add qrcode for the first address

--- src/app/payments/container.tsx
+++ src/app/payments/container.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
 import { DispatchProps, State, State as Props } from './types'
-
 const { connect } = require('react-redux')
+import { get } from 'lodash'
+import * as QRCode from 'qrcode.react'
 import { bindActionCreators, Dispatch } from 'redux'
 
 import { sendEca, setAmount, setToAddress } from './actions'
@@ -9,6 +10,7 @@ import { sendEca, setAmount, setToAddress } from './actions'
 // tslint:disable-next-line:typedef
 const mapStateToProps  = (state: State): Props =>
 ({
+  addresses: get(state, 'electra.electraJs.wallet.ADDRESSES', []),
   payments: state.payments
 })
 
@@ -38,12 +40,15 @@ export default class Payments extends React.Component<Props & DispatchProps, any
   }
 
   public render(): any {
+    const address: string = get(this.props, 'addresses[0].hash', '')
+
     return (
       <div>
         Payments
         <input type='text' placeholder='Address to send' onChange={this.setToAddress} />
         <input type='text' placeholder='Amount' onChange={this.setAmount} />
         <button onClick={this.onClick}>Send</button>
+        <QRCode value={address} />
       </div>
     )
   }
