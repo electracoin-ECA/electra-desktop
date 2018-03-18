905ff91396ea73c3904c665b6ea6c1d2ae83c355
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-03-18 19:48:22 +0100

    webpack: ensure safe paths resolution

--- webpack/config.path.js
+++ webpack/config.path.js
@@ -1,16 +1,14 @@
 const path = require('path')
 
 module.exports = {
-  assets: path.resolve('src/assets/images'),
-  buildPath: path.resolve('dist'),
-  entry: path.resolve('src/index.tsx'),
+  assets: path.resolve(__dirname, '../src/assets/images'),
+  buildPath: path.resolve(__dirname, '../dist'),
+  entry: path.resolve(__dirname, '../src/index.tsx'),
   entryMain: path.resolve(__dirname, '../main/index.ts'),
-  indexTemplate: path.resolve('src/index.html'),
-  libraryPath: path.resolve('library'),
-  stylePaths: [path.resolve('src/app/styles')],
-  sourcePath: path.resolve('src'),
+  indexTemplate: path.resolve(__dirname, '../src/index.html'),
+  libraryPath: path.resolve(__dirname, '../library'),
+  stylePaths: [path.resolve(__dirname, '../src/app/styles')],
+  sourcePath: path.resolve(__dirname, '../src'),
   tsconfigMain: path.resolve(__dirname, '../main/tsconfig.json'),
   tsconfigRenderer: path.resolve(__dirname, '../src/tsconfig.json'),
 }
-
-console.log(path.resolve(__dirname, '../main/index.ts'))
