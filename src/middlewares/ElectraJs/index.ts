8328e4500a6e320dc8e722bd71c8e01fd195c463
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-03-25 17:54:59 +0200

    middlewares/ElectraJs: switch to full abstract static

--- src/middlewares/ElectraJs/index.ts
+++ src/middlewares/ElectraJs/index.ts
@@ -5,14 +5,13 @@ import { bindEventToSyncCall } from './helpers'
 import Wallet from './wallet'
 import WebServices from './webServices'
 
-// tslint:disable-next-line:no-unnecessary-class
-export default class ElectraJsMiddleware {
+export default abstract class ElectraJsMiddleware {
   public static get constants(): ElectraJs['constants'] {
     return ipcRenderer.sendSync('electraJs:constants')
   }
 
-  public static wallet: Wallet = new Wallet()
-  public static webServices: WebServices = new WebServices()
+  public static wallet: Wallet = Wallet
+  public static webServices: WebServices = WebServices
 
   public static getVersion(): string {
     return bindEventToSyncCall<string>('electraJs:getVersion', arguments)
--- src/middlewares/ElectraJs/webServices.ts
+++ src/middlewares/ElectraJs/webServices.ts
@@ -2,12 +2,12 @@ import { CoinMarketCapCurrency } from 'electra-js'
 
 import { bindEventToAsyncCall } from './helpers'
 
-export default class WebServices {
-  public getBalanceFor(address: string): Promise<number> {
+export default abstract class WebServices {
+  public static getBalanceFor(address: string): Promise<number> {
     return bindEventToAsyncCall<number>('electraJs:getBalanceFor', arguments)
   }
 
-  public getCurrentPriceIn(currency?: CoinMarketCapCurrency): Promise<number> {
+  public static getCurrentPriceIn(currency?: CoinMarketCapCurrency): Promise<number> {
     return bindEventToAsyncCall<number>('electraJs:getCurrentPriceIn', arguments)
   }
 }
