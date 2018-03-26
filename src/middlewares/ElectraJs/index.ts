a1b55431019052590cb395f381e6a86fb2924dce
hloudin <hyjaz.loudin@gmail.com>
2018-03-26 08:44:03 -0400

    src/middlewares/ElectraJs: extend wallet class by webServices and in return extend ELectraJsMiddleware Class with Wallet

--- src/middlewares/ElectraJs/index.ts
+++ src/middlewares/ElectraJs/index.ts
@@ -3,17 +3,13 @@ import { ipcRenderer } from 'electron'
 
 import { bindEventToSyncCall } from './helpers'
 import Wallet from './wallet'
-import WebServices from './webServices'
 
-export default class ElectraJsMiddleware {
+export default class ElectraJsMiddleware extends Wallet {
   public static get constants(): ElectraJs['constants'] {
     return ipcRenderer.sendSync('electraJs:constants')
   }
 
   // tslint:disable-next-line:ban-types
-  public static wallet: Wallet
-  public static webServices: WebServices = new WebServices()
-
   public static getVersion(): string {
     return bindEventToSyncCall<string>('electraJs:getVersion', arguments)
   }
