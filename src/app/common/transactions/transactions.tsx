e3c1248285185af52972d274c70a51bd2e01c92e
hloudin <hyjaz.loudin@gmail.com>
2018-03-28 12:10:23 -0400

    src/app/common/transactions/transactions.tsx: fix transaction component

--- src/app/common/transactions/transactions.tsx
+++ src/app/common/transactions/transactions.tsx
@@ -1,3 +1,4 @@
+import { WalletTransaction } from 'electra-js'
 import { capitalize, reverse } from 'lodash'
 import * as moment from 'moment'
 import * as React from 'react'
@@ -5,26 +6,31 @@ import { Icon } from '../../icon'
 
 export default class TransactionsComponent extends React.Component<any, any> {
   public render(): any {
-    const { transactions } : any = this.props
+    const transactions : WalletTransaction[] = this.props.transactions
 
     return (
       <div className='mt-6 '>
-        {reverse(transactions).map((transaction: any, index: number) => (
+        {reverse(transactions).map((transaction: WalletTransaction, index: number) => (
           <div className='c-card mb-4' key={index}>
             <div className='c-card__content flex items-center'>
               <div className='flex-1'>
-                <h4>{transaction.address}</h4>
+                <h4>{transaction.from}</h4>
+                <h4>{transaction.to}</h4>
                 <span className='text-grey-light'>
                   <Icon name='calendar-alt' />&nbsp;
-                  {moment.unix(transaction.time).format('DD-MM-YYYY')}
+                  {moment.unix(transaction.date).format('DD-MM-YYYY')}
                 </span>
                 <span className='text-grey-light ml-4'>
                   <Icon name='clock' />&nbsp;
-                  {moment.unix(transaction.time).format('LT')}
+                  {moment.unix(transaction.date).format('LT')}
+                </span>
+                <br/>
+                <span className='text-grey-light ml-4'>
+                  Confirmation(s): {transaction.confimationsCount}
                 </span>
               </div>
               <div className='flex-none text-right'>
-                <span className='block'>{capitalize(transaction.category)}</span>
+                <span className='block'>{capitalize(transaction.type)}</span>
                 <p className='font-extra-bold'>{`${transaction.amount} ECA`}</p>
               </div>
             </div>
