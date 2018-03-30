2c2d97cff8f7664da0ecf70026c1f9934ce8eb11
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-03-30 06:02:51 +0200

    main: add tray item & minimize / close behaviors

--- main/index.ts
+++ main/index.ts
@@ -1,19 +1,19 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Event, Menu, Tray } from 'electron'
 import * as path from 'path'
 import * as url from 'url'
 
 import Communication from './communication'
 
+let isHidden: boolean = false
+const isProd: boolean = process.env.NODE_ENV === 'production'
+let isQuiting: boolean = false
+let mainWindow: BrowserWindow
+
 // Start IPC events bus listener
 const communication: Communication = new Communication()
 
-let mainWindow: BrowserWindow | null
-
-// const isDev: boolean =
-//   process.defaultApp
-//   || /[\\/]electron-prebuilt[\\/]/.test(process.execPath)
-//   || /[\\/]electron[\\/]/.test(process.execPath)
-const isProd: boolean = process.env.NODE_ENV === 'production'
+// Instantiate tray item
+let tray: Tray
 
 function createWindow(): void {
   // Create the browser window.
@@ -49,20 +49,71 @@ function createWindow(): void {
     if (!isProd) mainWindow.webContents.openDevTools()
   })
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
+  mainWindow.on('minimize', (event: Event) => {
+    event.preventDefault()
+    toggleMainWindows()
+  })
+
+  mainWindow.on('close', (event: Event) => {
+    if (isQuiting) return
+
+    event.preventDefault()
+    toggleMainWindows()
   })
 }
 
-app.on('ready', createWindow)
+function updateTray(): void {
+  const contextMenu: Menu = Menu.buildFromTemplate(
+    (isHidden
+      ? [
+        {
+          click: toggleMainWindows,
+          label: 'Show Electra Desktop',
+        },
+      ]
+      : [
+        {
+          click: toggleMainWindows,
+          label: 'Mimize to Tray',
+        },
+      ]
+    ).concat(
+      [
+        {
+          click: async (): Promise<void> => {
+            isQuiting = true
+            console.info('Closing Electra daemon...')
+            await communication.electraJs.wallet.stopDaemon()
 
-app.on('window-all-closed', async () => {
-  console.info('Closing Electra daemon...')
-  await communication.electraJs.wallet.stopDaemon()
+            if (process.platform !== 'darwin') app.quit()
+          },
+          label: 'Exit',
+        },
+      ]
+    )
+  )
+  tray.setContextMenu(contextMenu)
+}
 
-  if (process.platform !== 'darwin') app.quit()
-})
+function toggleMainWindows(): void {
+  if (isHidden) {
+    mainWindow.show()
+    isHidden = false
+    updateTray()
 
-app.on('activate', () => {
-  if (mainWindow === null) createWindow()
+    return
+  }
+
+  mainWindow.hide()
+  isHidden = true
+  updateTray()
+}
+
+app.once('ready', () => {
+  tray = new Tray(path.resolve(__dirname, '../renderer/assets/logo.png'))
+  tray.setToolTip('Electra Desktop')
+  tray.on('click', toggleMainWindows)
+
+  updateTray()
+  createWindow()
 })
