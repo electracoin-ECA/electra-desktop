c7c3091db6dd8f7c42448d7f8641c1a79c579dd3
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-04-03 09:32:26 +0200

    main/communication/helpers: improve logs

--- main/communication/helpers.ts
+++ main/communication/helpers.ts
@@ -1,43 +1,47 @@
 import to from 'await-to-js'
 import { ipcMain } from 'electron'
+import log from 'electron-log'
 
 const isProd: boolean = process.env.NODE_ENV === 'production'
 
+// Logs level
+log.transports.file.level = isProd ? false : 'debug'
+
 export function bindEventToProp(eventName: string, instance: any, prop: string): void {
   ipcMain.on(eventName, (event: any) => {
-    if (!isProd) console.info(`ipcMain: ${eventName}`)
+    log.debug(`ipcMain: ${eventName}`)
     event.returnValue = JSON.stringify(instance[prop])
   })
 }
 
 export function bindEventToAsyncCall(eventName: string, call: () => Promise<any>): void {
   ipcMain.on(eventName, async (event: any, argsString: string) => {
-    if (!isProd) console.info(`ipcMain: ${eventName}`)
+    log.debug(`ipcMain: ${eventName}`)
     const [err, res] = await to(call.apply(null, JSON.parse(argsString)))
     if (err !== null) {
-      if (!isProd) console.info(`ipcMain: ${eventName}:error`, err)
+      log.debug(`ipcMain: ${eventName}:error`, err)
       event.sender.send(`${eventName}:error`, typeof err === 'string' ? err : err.message)
 
       return
     }
 
-    if (!isProd) console.info(`ipcMain: ${eventName}:success`, res)
+    log.debug(`ipcMain: ${eventName}:success`, res)
     event.sender.send(`${eventName}:success`, JSON.stringify(res))
   })
 }
 
 export function bindEventToSyncCall(eventName: string, call: () => any): void {
   ipcMain.on(eventName, (event: any, argsString: string) => {
-    if (!isProd) console.info(`ipcMain: ${eventName}`)
+    log.debug(`ipcMain: ${eventName}`)
 
     try {
       // tslint:disable-next-line:no-shadowed-variable
       const res: any = call.apply(null, JSON.parse(argsString))
-      if (!isProd) console.info(`ipcMain: ${eventName}:success`, res)
+      log.debug(`ipcMain: ${eventName}:success`, res)
       event.returnValue = res === undefined ? '' : JSON.stringify(res)
     }
     catch (err) {
-      if (!isProd) console.info(`ipcMain: ${eventName}:error`, err)
+      log.debug(`ipcMain: ${eventName}:error`, err)
       event.sender.send(`${eventName}:error`, typeof err === 'string' ? err : err.message)
     }
   })
