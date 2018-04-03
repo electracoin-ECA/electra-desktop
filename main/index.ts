26fba6ed37d32998c3bb7322e46ea2d50d0373f1
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-04-03 09:40:55 +0200

    main: prepare auto-update

--- main/index.ts
+++ main/index.ts
@@ -1,4 +1,8 @@
-import { app, BrowserWindow, Event, ipcMain, Menu, Tray } from 'electron'
+// tslint:disable-next-line:no-implicit-dependencies
+import { ProgressInfo } from 'builder-util-runtime'
+import { app, BrowserWindow, Event, Menu, Tray } from 'electron'
+import log from 'electron-log'
+import { autoUpdater } from 'electron-updater'
 import * as path from 'path'
 import * as url from 'url'
 const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer')
@@ -11,15 +15,17 @@ const isProd: boolean = process.env.NODE_ENV === 'production'
 let isQuiting: boolean = false
 let mainWindow: BrowserWindow
 
+// Logs level
+log.transports.file.level = isProd ? false : 'debug'
+
+// Auto-update logs
+autoUpdater.logger = log
+
 // Start IPC events bus listener
 const communication: Communication = new Communication()
 
 // Instantiate tray item
 let tray: Tray
-
-// Allow main process to send events to renderer process
-let rendererProcessSender: any
-ipcMain.on('app:quit:listen', (event: any) => rendererProcessSender = event.sender)
 
 function createWindow(): void {
   // Create the browser window.
@@ -54,6 +60,9 @@ function createWindow(): void {
 
     mainWindow.show()
     if (!isProd) mainWindow.webContents.openDevTools()
+
+    // Check for updates
+    autoUpdater.checkForUpdatesAndNotify()
   })
 
   mainWindow.on('minimize', (event: Event) => {
@@ -100,13 +109,13 @@ async function exitApp(): Promise<void> {
   isQuiting = true
 
   try {
-    rendererProcessSender.send('app:quit')
-    console.info('Closing Electra daemon...')
+    mainWindow.webContents.send('app:quit')
+    log.info('Closing Electra daemon...')
     await communication.electraJs.wallet.stopDaemon()
     app.quit()
   }
   catch(err) {
-    console.error(err)
+    log.error(err)
     app.quit()
   }
 }
@@ -125,20 +134,46 @@ function toggleMainWindows(): void {
   updateTray()
 }
 
+autoUpdater.on('checking-for-update', () => {
+  log.debug(`electron-updater: Checking for update...`)
+  mainWindow.webContents.send('app:autoUpdate:checking')
+})
+autoUpdater.on('update-available', (info: any) => {
+  log.debug('electron-updater: An update has been found.', info)
+  mainWindow.webContents.send('app:autoUpdate:found', JSON.stringify(info))
+})
+autoUpdater.on('update-not-available', (info: any) => {
+  log.debug('electron-updater: No update has been found.', info)
+  mainWindow.webContents.send('app:autoUpdate:notFound', JSON.stringify(info))
+})
+autoUpdater.on('error', (err: any) => {
+  log.error('electron-updater: An error happened.', err)
+  mainWindow.webContents.send('app:autoUpdate:error', JSON.stringify(err))
+})
+autoUpdater.on('download-progress', (progressObj: ProgressInfo) => {
+  log.error('electron-updater: Downloading update...', progressObj)
+  mainWindow.webContents.send('app:autoUpdate:progress', JSON.stringify(progressObj))
+})
+autoUpdater.on('update-downloaded', (info: any) => {
+  log.debug('electron-updater: Update downloaded.', info)
+  mainWindow.webContents.send('app:autoUpdate:downloaded', JSON.stringify(info))
+})
+
 app.once('ready', () => {
+  log.info('Electra Desktop starting...')
+
   tray = new Tray(path.resolve(__dirname, 'assets/icons/tray.png'))
   tray.setToolTip('Electra Desktop')
   tray.on('click', toggleMainWindows)
-
   updateTray()
 
   if (isHot) {
     installExtension(REACT_DEVELOPER_TOOLS)
       .then((name: any) => {
-        console.info(`Added Extension: ${name}`)
+        log.info(`Added Extension: ${name}`)
         createWindow()
       })
-      .catch(console.error)
+      .catch(log.error)
 
     return
   }
