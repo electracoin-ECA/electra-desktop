4fc79c789cb6635786955e39923cdfd9483bcb6d
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-04-17 19:00:34 +0200

    s

--- src/helpers/waitFor.ts
+++ src/helpers/waitFor.ts
@@ -2,5 +2,5 @@
  * Wait for <inMs> milliseconds.
  */
 export default async function(inMs: number): Promise<void> {
-  return new Promise<void>((resolve: () => void): NodeJS.Timer => setTimeout(resolve, forInMs))
+  return new Promise<void>((resolve: () => void): NodeJS.Timer => setTimeout(resolve, inMs))
 }
