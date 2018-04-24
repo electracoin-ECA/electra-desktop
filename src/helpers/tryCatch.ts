9e4cca3587d13bfdceff12bb23f8d31a3ff2156d
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-04-24 03:09:20 +0200

    helpers/tryCatch: lint

--- src/helpers/tryCatch.ts
+++ src/helpers/tryCatch.ts
@@ -1,6 +1,6 @@
 import throwError, { ERROR } from './throwError'
 
-export default function<T = undefined>(promise: Promise<T>, errorCode: keyof typeof ERROR): Promise<T | void> {
+export default async function<T = undefined>(promise: Promise<T>, errorCode: keyof typeof ERROR): Promise<T | void> {
   return promise
     .then((data: T | undefined) => data)
     .catch((err: Error | string) => {
