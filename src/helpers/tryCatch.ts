b6c5d306f838e42a4f17223fbfe6f5e081128105
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-04-24 02:46:07 +0200

    app: improve errors automation & handling

---
+++ src/helpers/tryCatch.ts
@@ -0,0 +1,9 @@
+import throwError, { ERROR } from './throwError'
+
+export default function<T = undefined>(promise: Promise<T>, errorCode: keyof typeof ERROR): Promise<T | void> {
+  return promise
+    .then((data: T | undefined) => data)
+    .catch((err: Error | string) => {
+      throwError(errorCode, typeof err === 'string' ? new Error(err) : err)
+    })
+}
