8faecea22152f1e929b8678c87d24aff715cc7d9
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-04 09:41:09 +0200

    app/store: lint

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,7 @@
 import * as _ from 'lodash'
 import { Action, applyMiddleware, combineReducers, createStore, Middleware, Reducer } from 'redux'
 import logger from 'redux-logger'
-import { combineEpics, createEpicMiddleware, Epic, EpicMiddleware } from 'redux-observable'
+import { combineEpics, createEpicMiddleware, EpicMiddleware } from 'redux-observable'
 
 import epics from './epics'
 import * as reducers from './reducers'
@@ -9,7 +9,7 @@ import { StoreState } from './types'
 
 const epicMiddleware: EpicMiddleware<Action<any>, Action<any>, void, any> = createEpicMiddleware()
 const rootReducer: Reducer<StoreState> = combineReducers({ ...reducers })
-const rootEpic: Epic<any, any, any, any> = combineEpics(..._.values(epics))
+const rootEpic = combineEpics(..._.values(epics))
 
 const middlewares: Middleware[] = []
 if (process.env.NODE_ENV !== 'production') middlewares.push(logger)
