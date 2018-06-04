f90e9f75fd72819fb88e94c4ab6db99da0e2065f
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-04 09:41:28 +0200

    app/epics: lint

--- src/app/epics.ts
+++ src/app/epics.ts
@@ -1,5 +1,4 @@
 import * as _ from 'lodash'
-import { Epic } from 'redux-observable'
 
 import { accountEpics } from './account'
 import { ToastEpic } from './common/toast'
@@ -8,7 +7,7 @@ import { headerEpics } from './header'
 import { loginEpics } from './login'
 import { paymentsEpics } from './payments'
 
-const epics: Array<Epic<any, any, any, any>> = _.reduce(
+const epics = _.reduce(
   [accountEpics, headerEpics, loginEpics, paymentsEpics, ToastEpic, unlockModalEpics],
   (result: any, epic: any) => _.concat(result, _.values(epic)),
   [],
