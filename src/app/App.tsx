2f0c3e357a939a7c4d5f3a3f3ff9d481552bef87
hloudin <hyjaz.loudin@gmail.com>
2018-06-06 03:21:45 -0400

    App.tsx: move redirect react component inside switch component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -127,8 +127,6 @@ class App extends React.Component<StoreState, OwnState> {
                 <Sidebar />
               </aside>
               <main>
-                {/* TODO Fix this dirty hack to handle missing redirect on production app. */}
-                <Redirect to='/' />
                 <Switch>
                   <Route exact path='/' component={AccountOverview} />
                   <Route exact path='/purse' component={AccountPurse} />
@@ -138,6 +136,7 @@ class App extends React.Component<StoreState, OwnState> {
                   <Route exact path='/payments' component={Payments} />
                   <Route exact path='/address_book' component={AddressBook} />
                   <Route exact path='/settings' component={Settings} />
+                  <Redirect to='/' />
                 </Switch>
                 <Toast
                   message={this.props.toast.message}
