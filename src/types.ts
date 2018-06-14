7ff5e3f9913858d36c9d63a2aee48bb69e12498e
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-14 22:30:48 +0200

    types: migrate to typescript v2.19

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 import { WalletStartDataHard } from 'electra-js'
 
 export type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T]
-export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>
+export type Omit<T, K extends Extract<keyof T, string>> = Pick<T, Diff<Extract<keyof T, string>, K>>
 
 export interface UserSettings extends WalletStartDataHard {
   appId: string
